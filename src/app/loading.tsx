7561ba3149ccad6267892281e44ffb604c6d7a14
ManatoYamashita <mail@yamashitamana.to>
2026-09-18 11:17:24 +0900

    FIX: お知らせ未存在ページをHTTP 404にする

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loading } from "@/components/ui/Loading";
-
-export default function RootLoading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Loading label="読み込み中..." />
-    </div>
-  );
-}
